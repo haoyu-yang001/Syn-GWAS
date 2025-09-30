cfae128d52c5e5e3d0dbce5352aefac8bf8528a6
haoyu-yang001 <yanghaoyu000720@gmail.com>
2025-09-30 15:38:30 -0400

    Update blocks.R

--- R/blocks.R
+++ R/blocks.R
@@ -1,31 +1,33 @@
 find_blocks_vectorized <- function(A) {
   n <- nrow(A)
-  
-  sm <- summary(A)
 
-  max_j <- rep(0, n)
-  tmp <- tapply(sm$j, sm$i, max)
-  max_j[as.integer(names(tmp))] <- tmp
-  
+  # 统一拿 (i, j, x) 三元组
+  if (inherits(A, "sparseMatrix")) {
+    sm <- Matrix::summary(A)   # 确保用到 Matrix 的方法
+  } else {
+    nz <- which(A != 0, arr.ind = TRUE)
+    sm <- data.frame(i = nz[,1], j = nz[,2], x = A[nz], row.names = NULL)
+  }
+
+  max_j <- integer(n)
+  if (nrow(sm) > 0) {
+    tmp <- tapply(sm$j, sm$i, max)
+    max_j[as.integer(names(tmp))] <- tmp
+  }
   zero_rows <- which(max_j == 0)
-  if(length(zero_rows) > 0){
-    max_j[zero_rows] <- zero_rows
-  }
-  
+  if (length(zero_rows) > 0) max_j[zero_rows] <- zero_rows
+
   cum_max <- cummax(max_j)
-  
-  block_boundaries <- which(seq_len(n) == cum_max)
-  
-  blocks <- vector("list", length(block_boundaries))
-  start <- 1
-  for (i in seq_along(block_boundaries)) {
-    end <- block_boundaries[i]
-    blocks[[i]] <- start:end
-    start <- end + 1
+  bnd <- which(seq_len(n) == cum_max)
+
+  blocks <- vector("list", length(bnd))
+  start <- 1L
+  for (k in seq_along(bnd)) {
+    end <- bnd[k]; blocks[[k]] <- start:end; start <- end + 1L
   }
-  
-  return(blocks)
+  blocks
 }
+
 
 find_blocks_vectorized_threshold <- function(A, threshold = 0.005) {
   n <- nrow(A)
