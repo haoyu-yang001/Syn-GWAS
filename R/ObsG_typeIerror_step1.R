eba0532c7c1ca9a41d472cbee5ae072c090cbebe
haoyu-yang001 <yanghaoyu000720@gmail.com>
2025-09-30 14:52:12 -0400

    Create ObsG_typeIerror_step1.R

---
+++ R/ObsG_typeIerror_step1.R
@@ -0,0 +1,51 @@
+ObsG_typeIerror_step1 <- function(mydf) {
+  
+  GRM <- mydf$GRM
+  
+  test_df <- data.frame(X = mydf$X_all, Y_all = mydf$Y, Y_obs = mydf$Y_obs, yhat = mydf$S)
+  
+  ## obtain the submatrix of GRM
+  obs_protein_index <- which(!is.na(test_df$Y_obs))
+  GRM_obs_obs <- GRM[obs_protein_index, obs_protein_index]
+  
+  
+  data_obs <- data.frame(
+    y = test_df$Y_obs,
+    x = test_df$X
+  )%>% na.omit()
+  
+  n_obs <- nrow(data_obs)
+  
+  model_lm <- lm(y ~ x, data = data_obs)
+  
+  GRM_o <- GRM_obs_obs
+  diag(GRM_o) <- 0
+  
+  a1 <- as.numeric(t(model_lm$residuals) %*% GRM_o %*% model_lm$residuals)
+  hat_tau_T2 <- a1 / sum((GRM_o)^2) #equal to sum(diag((GRM_o) %*% t(GRM_obs_obs)))
+  
+  a2 <- sum(model_lm$residuals * model_lm$residuals)
+  hat_sigma_T2 <- (a2 - hat_tau_T2 * sum(diag(GRM_obs_obs)))/nrow(GRM_obs_obs)
+  
+  Sigma11 <- hat_tau_T2 * GRM_obs_obs + hat_sigma_T2 * Diagonal(n_obs)
+  inv_Sigma11 <- matrix_inv_block(wait_matrix=Sigma11)
+  
+  ## for obs or oracle test statistic X needs to include the intercept!
+  Y <- data_obs$y
+  X <- cbind(rep(1,length(Y)),data_obs$x)
+  
+  SX <- inv_Sigma11 %*% X
+  XtSX_inv <- solve(crossprod(X, SX))
+  beta_hat <- XtSX_inv %*% (t(SX) %*% Y)
+  residual <- Y - X %*% beta_hat
+  invS11_res <- inv_Sigma11 %*% residual
+  
+  params <- c(
+    "obs_protein_index","invS11_res","SX","inv_Sigma11","XtSX_inv"
+  )
+  
+  ObsG_typeIerror_step1_pars <- mget(params, envir = environment())
+  
+  return(ObsG_typeIerror_step1_pars)
+  
+}
